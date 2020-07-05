fb294eb115019bc146469c8b3e47cb43974d68e2
Prakhathi <prakhathi@twinelabs.com>
2020-07-05 18:44:57 -0400

    Updates

---
+++ precache-manifest.6cb4d3e634fad8eb259743aa83a42455.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b186c36ee9e14ac6312a16f22a76bca",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "0736192bef530fef5daf",
+    "url": "/Portfolio/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "c8858fe284090ddbd87a",
+    "url": "/Portfolio/static/css/main.d58d94d5.chunk.css"
+  },
+  {
+    "revision": "0736192bef530fef5daf",
+    "url": "/Portfolio/static/js/2.ddea9803.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.ddea9803.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8858fe284090ddbd87a",
+    "url": "/Portfolio/static/js/main.67eab8eb.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "825c91cffcb07f4b49bef2cc5bc818a6",
+    "url": "/Portfolio/static/media/bg.825c91cf.jpg"
+  },
+  {
+    "revision": "1a8e6ff283e8197b7e4d220c302a749c",
+    "url": "/Portfolio/static/media/profile_pic.1a8e6ff2.jpg"
+  }
+]);
